b3025ec3a2ec628de8551a8c645d9faf767861fa
Seulgi Kim <seulgi.kim@codechain.io>
2020-01-02 15:13:24 +0900

    Run cargo fix --edition-idioms

--- src/lib.rs
+++ src/lib.rs
@@ -14,10 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-extern crate kvdb;
-extern crate primitives;
-extern crate rlp;
-
 #[cfg(test)]
 extern crate codechain_crypto as crypto;
 #[cfg(test)]
